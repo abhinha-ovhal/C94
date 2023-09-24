f2c35bb2c3dc428ab15556ce2c5d6ec846ee009a
abhinha-ovhal <133642974+abhinha-ovhal@users.noreply.github.com>
2023-09-24 15:19:33 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAgZNVwUrKM4EDs8P05DM43AtSiOuaieqY",
+    authDomain: "project-kwitter-cd7e7.firebaseapp.com",
+    projectId: "project-kwitter-cd7e7",
+    storageBucket: "project-kwitter-cd7e7.appspot.com",
+    messagingSenderId: "645583595000",
+    appId: "1:645583595000:web:a1ba946041fbaaaf5d620d"
+  };
+  
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
